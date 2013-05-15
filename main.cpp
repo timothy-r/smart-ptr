0e6dad3a787403cc6bb5f095f07411079116eb8f
timothy-r <tim.rodger@gmail.com>
2013-05-15 21:06:08 +0100

    removed commented out code

--- main.cpp
+++ main.cpp
@@ -11,20 +11,16 @@ int main()
     SmartPtr<Person> p(new Person("Tim", 47));
     showPerson(*p);
 
-    //p->Display();
     {
         SmartPtr<Person> q = p;
         showPerson(*q);
-        //q->Display();
     }
 
     SmartPtr<Person> r(new Person("Rodger", 7));
-    //r->Display();
     showPerson(*r);
 
     r = p;
     showPerson(*r);
-    //r->Display();
 
     return 0;
 }
